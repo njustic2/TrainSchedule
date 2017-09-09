71c303ad254d8b46e8b80b59471ff0a2b61bd949
Nathan <natwave2@gmail.com>
2017-09-09 11:09:32 -0600

    commit

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,6 +1,6 @@
 // FireBase Link =======================================================================================================
   // Initialize Firebase
-  var config = {
+  var config = { //This is the firebase park
     apiKey: "AIzaSyClgOv_9CtEMJzdPPwtXu_Cfw8ud-fYNQI",
     authDomain: "trainschedule-92b95.firebaseapp.com",
     databaseURL: "https://trainschedule-92b95.firebaseio.com",
@@ -8,9 +8,9 @@
     storageBucket: "trainschedule-92b95.appspot.com",
     messagingSenderId: "538788932449"
 };
-firebase.initializeApp(config);
-var database = firebase.database();
-var trainsRef = firebase.database().ref().child('trains');
+firebase.initializeApp(config);// This part starts the firebase application
+var database = firebase.database();// This part stores the information as a database
+var trainsRef = firebase.database().ref().child('trains');// This part adds the information typed into the firebase database
 // Global Variables ====================================================================================================
 
 var trainName;
@@ -38,21 +38,28 @@ $(document).ready(function() {
                 '<td>' + destination + '</td>' +
                 '<td>' + trainTime + '</td>' +
                 '<td>' + frequency + '</td>' + 
-                '<td>' + getNextTrainTime(trainTime, frequency) + '</td>' +
-                '<td>Figure Out</td>' +
+                '<td>' + nextTrainTime.format('HH:mm') + '</td>' +
+                '<td>' + getNextArrival(nextTrainTime) + '</td>' +
             '</tr>'
         );
 
         // //Moment.js Variables ================================================================================================    
         function getNextTrainTime(trainTime, frequency) {
-            var firstTimeConverted = moment(trainTime, "hh:mm");    // First train time converted to miltary time
+            var firstTimeConverted = moment(trainTime, "hh:mm");// First train time converted to miltary time
             var currentTime = moment();    // Current Time
-            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");    // Difference between the times
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");//Difference between the times
             var tRemainder = diffTime % frequency;    // Time apart (remainder)
             var tMinutesTillTrain = frequency - tRemainder;     // Minute Until Train
             var nextTrain = moment().add(tMinutesTillTrain, "minutes");    // Next Train
-            return nextTrain.format('HH:mm');
+            return nextTrain;
         }
+        
+        function getNextArrival(nextArrival) {// This function finds the difference between the current time and the next arrival
+            var currentTime = moment();// Current Time, as in now
+            var diffTime = moment(nextArrival).diff(moment(), "minutes"); //Difference between the now time and the next arrival time
+            return diffTime;// Returns the that difference
+        }
+
     });
 
 
